cea7cf347b8f6d992290189cb0bbabc7f4046334
pujan5555 <puzanchakma@gmail.comm>
2025-05-04 18:04:38 +0600

    almost

--- script.js
+++ script.js
@@ -9,11 +9,86 @@ const clearButton = document.getElementById("clear-btn");
 const loading = document.getElementById("loading");
 const viewMoreBtn = document.getElementById("view-more-btn");
 const scrollSection = document.getElementById("scroll-section");
-const petModal = document.getElementById('pet-modal');
-const modalBox = document.getElementById('modal-box');
+const petModal = document.getElementById("pet-modal");
+const modalBox = document.getElementById("modal-box");
+const sort = document.getElementById("sort");
 
-petModal.classList.add("flex", "flex-col", "gap-2", "overflow-scroll", "h-[600px]", "max-sm:py-2", "max-sm:h-[300px]");
-modalBox.classList.add('rounded-md');
+// sorting
+sort.addEventListener("click", async () => {
+    petMain.innerHTML = "";
+    const response = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
+    const info = await response.json();
+    const data = info.pets;
+    const priceList = [];
+    const petID = [];
+    data.forEach(element => {
+        const price = element.price;
+        const id = element.petId;
+        priceList.push(price);
+        petID.push(id);
+    });
+    const sortobj = Object.fromEntries(petID.map((id, i) => [id, priceList[i]]));
+    const sortPriceList = (Object.values(sortobj).sort((x, y) => x - y));
+    for (let i = 0; i < data.length; i++) {
+        const id = Object.keys(sortobj).find(key => sortobj[key] === sortPriceList[i]);
+        LoadById(id);
+    }
+});
+async function LoadById(id) {
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/pet/'+id);
+    const info = await res.json();
+    const data = info.petData;
+    const petCard = document.createElement('div');
+    petCard.classList.add(
+        "flex",
+        "flex-col",
+        "items-start",
+        "justify-center",
+        "gap-3",
+        "p-4",
+        "border-2",
+        "border-blue-100",
+        "rounded-xl",
+        "max-sm:w-full"
+    );
+    petCard.innerHTML = `
+                <img src ="${data.image}" alt="pets" class="rounded-xl object-cover h-full"/>
+                <p class="text-2xl font-bold">${data.pet_name}</p>
+                <div class="flex items-center gap-2 justify-start w-full">
+                    <img src="https://img.icons8.com/?size=100&id=2bbPBbZvbi8l&format=png&color=131313" alt="pets" class="w-6 h-6"/>
+                    <p class="text-md text-[#131313]">Breed: ${data.breed}</p>
+                </div>
+                <div class="flex items-center gap-2 justify-start w-full">
+                    <img src="https://img.icons8.com/?size=100&id=vwGXRtPWrZSn&format=png&color=131313" alt="pets" class="w-6 h-6"/>
+                    <p class="text-md text-[#131313]">Birth: ${data.date_of_birth}</p>
+                </div>
+                <div class="flex items-center gap-2 justify-start w-full">
+                    <img src="https://img.icons8.com/?size=100&id=Kv6q3DKYDp1T&format=png&color=131313" alt="pets" class="w-6 h-6"/>
+                    <p class="text-md text-[#131313]">Gender: ${data.gender}</p>
+                </div>
+                <div class="flex items-center gap-2 justify-start w-full">
+                    <img src="https://img.icons8.com/?size=100&id=0oWpxDgVkkru&format=png&color=131313" alt="pets" class="w-6 h-6"/>
+                    <p class="text-md text-[#131313] flex">Price: ${data.price} <img src="https://img.icons8.com/?size=100&id=0oWpxDgVkkru&format=png&color=131313" class="w-6 h-6"/></p>
+                </div>
+                <div class="flex items-center gap-2 justify-around w-full">
+                    <button class=" text-blue-400 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600 transition-all border-2 border-blue-300" id="p"> <img src="https://img.icons8.com/?size=100&id=u8MTpAq972MG&format=png&color=60a5fa" class="w-6 h-6"/> </button>
+                    <button class=" text-blue-400 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600 transition-all border-2 border-blue-300" id="adopt">Adopt</button>
+                    <button class="text-blue-400 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600 transition-all border-2 border-blue-300" onclick="my_modal.showModal()" id = "details">Details</button>
+                </div>`;
+    petMain.append(petCard);
+}
+// sorting
+
+petModal.classList.add(
+    "flex",
+    "flex-col",
+    "gap-2",
+    "overflow-scroll",
+    "h-[600px]",
+    "max-sm:py-2",
+    "max-sm:h-[300px]"
+);
+modalBox.classList.add("rounded-md");
 
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("hidden");
@@ -132,7 +207,7 @@ const petCardFunction = async (
                 </div>
                 <div class="flex items-center gap-2 justify-around w-full">
                     <button class=" text-blue-400 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600 transition-all border-2 border-blue-300" id="${petId}"> <img src="https://img.icons8.com/?size=100&id=u8MTpAq972MG&format=png&color=60a5fa" class="w-6 h-6"/> </button>
-                    <button class=" text-blue-400 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600 transition-all border-2 border-blue-300">Adopt</button>
+                    <button class=" text-blue-400 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600 transition-all border-2 border-blue-300" id="adopt${petId}">Adopt</button>
                     <button class="text-blue-400 px-4 py-2 rounded-lg hover:text-blue-600 hover:border-blue-600 transition-all border-2 border-blue-300" onclick="my_modal.showModal()" id = "details${petId}">Details</button>
                 </div>
             `;
@@ -185,6 +260,22 @@ const petCardFunction = async (
                     <h1 class = "font-bold text-lg">Details Information:</h1>
                     <p class="text-center">${element.pet_details}</p>
             </div>`;
+        });
+        document.getElementById(`adopt${petId}`).addEventListener("click", (e) => {
+            e.target.innerHTML = "Adopted";
+            e.target.disabled = true;
+            e.target.classList.remove(
+                "text-blue-400",
+                "px-4",
+                "py-2",
+                "rounded-lg",
+                "hover:text-blue-600",
+                "hover:border-blue-600",
+                "transition-all",
+                "border-2",
+                "border-blue-300"
+            );
+            e.target.classList.add("cursor-not-allowed");
         });
     });
     if (petMain.children.length === 0) {
